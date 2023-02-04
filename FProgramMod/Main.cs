f7a80d35f2e1f9a0601614c00c03b377d52e3932
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2023-02-04 17:01:30 -0500

    Working on running scripts from the Scripts folder.

--- FProgramMod/Main.cs
+++ FProgramMod/Main.cs
@@ -9,9 +9,12 @@ using Type = SFS.UI.ModGUI.Type;
 using System;
 using System.IO;
 using Python.Runtime;
+using System.Runtime.CompilerServices;
+using UnityEngine.Serialization;
+using System.Reflection;
+
 namespace SFSMod
 {
-
     /**
      * You only need to implement the Mod class once in your mod. The Mod class is how 
      * you tell the mod loader what it needs to load and execute.
@@ -38,16 +41,93 @@ namespace SFSMod
             window.EnableScrolling(Type.Vertical);
             int i = 1;
             foreach (string fname in Directory.GetFiles("Scripts/")) {
-                Debug.Log(fname);
-                Main.scriptFilename = fname;
-                Builder.CreateButtonWithLabel(window, 290, 30, 0, 70 * i, fname, "Load Script", StartScript(fname));
-                Builder.CreateSeparator(window, 300, 0, 70 * (i + 1));
-                i++;
+                if(!(fname is "Scripts/script_loader.py"))
+                {
+                    Debug.Log(fname);
+                    Main.scriptFilename = fname;
+                    string localFname = fname;
+                    SFSFP.CreateButtonWithLabel(window, 290, 30, 0, 70 * i, fname, "Load Script", SFSFP.RunScript, localFname);
+                    Builder.CreateSeparator(window, 300, 0, 70 * (i + 1));
+                    i++;
+                }
+                
             }
 
             
 
         }
+        public static ButtonWithLabel CreateButtonWithLabel(Transform parent, int width, int height, int posX = 0, int posY = 0, string labelText = "", string buttonText = "", Action<string> onClick = null, string argument = "")
+        {
+            try
+            {
+                System.Type modGUIPrefabsLoaderType = System.Type.GetType("SFS.UI.ModGUI.ModGUIPrefabsLoader, Assembly-CSharp");
+                if (modGUIPrefabsLoaderType != null)
+                {
+                    FieldInfo mainField = modGUIPrefabsLoaderType.GetField("main", BindingFlags.NonPublic | BindingFlags.Static);
+                    if (mainField != null)
+                    {
+                        var modGUIPrefabsLoaderInstance = mainField.GetValue(null);
+                        FieldInfo buttonWithLabelPrefabField = modGUIPrefabsLoaderType.GetField("buttonWithLabelPrefab", BindingFlags.Public | BindingFlags.Instance);
+                        if (buttonWithLabelPrefabField != null)
+                        {
+                            var buttonWithLabelPrefab = buttonWithLabelPrefabField.GetValue(modGUIPrefabsLoaderInstance);
+                            GameObject self = UnityEngine.Object.Instantiate((GameObject)buttonWithLabelPrefab);
+                            ButtonWithLabel buttonWithLabel = new ButtonWithLabel();
+                            buttonWithLabel.Init(self, parent);
+                            buttonWithLabel.Size = new Vector2(width, height);
+                            buttonWithLabel.Position = new Vector2(posX, posY);
+                            buttonWithLabel.label.Text = labelText;
+                            buttonWithLabel.button.Text = buttonText;
+                            if (onClick != null)
+                            {
+                                buttonWithLabel.button.OnClick = (Action) (() => { try { string lfname = argument; Debug.Log("fcall"); onClick((string)lfname); } catch (Exception err) { Debug.Log(err); } });
+                            }
+
+                            return buttonWithLabel;
+                        }
+                        Debug.Log("prefab not found");
+                        return null;
+                    }
+                    Debug.Log("main not found");
+                    return null;
+                }
+                Debug.Log("Type not found");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                return null;
+            }
+        }
+        /*
+        public static ButtonWithLabel CreateButtonWithLabel<T>(Transform parent, int width, int height, int posX = 0, int posY = 0, string labelText = "", string buttonText = "", Action<T> onClick = null, T argument = default(T))
+        {
+            System.Type type = System.Type.GetType("SFS.UI.ModGUI.ModGUIPrefabsLoader, Assembly-CSharp");
+            FieldInfo field = type.GetField("main.buttonWithLabelPrefab", BindingFlags.NonPublic | BindingFlags.Static);
+            if (field != null)
+            {
+                GameObject prefab = (GameObject)field.GetValue(null);
+                Debug.Log(prefab);
+                GameObject self = UnityEngine.Object.Instantiate((UnityEngine.GameObject)prefab);//.buttonWithLabelPrefab);
+                ButtonWithLabel buttonWithLabel = new ButtonWithLabel();
+                buttonWithLabel.Init(self, parent);
+                buttonWithLabel.Size = new Vector2(width, height);
+                buttonWithLabel.Position = new Vector2(posX, posY);
+                buttonWithLabel.label.Text = labelText;
+                buttonWithLabel.button.Text = buttonText;
+                if (onClick != null)
+                {
+                    buttonWithLabel.button.OnClick = () => { onClick(argument); };
+                }
+
+                return buttonWithLabel;
+            }
+            Debug.Log("type==null");
+            return null;
+
+        }*/
+
         // this ModNameID can be whatever you want
         public override string ModNameID => "amgongus";
 
@@ -111,24 +191,29 @@ namespace SFSMod
         static void RunScript (string scriptName)
         {
             Debug.Log("starting script " + scriptName);
-            //MsgDrawer.main.Log("Loading script: " + scriptName);
+            MsgDrawer.main.Log("Loading script: " + scriptName);
             Runtime.PythonDLL = "./python310.dll";
             PythonEngine.Initialize();
-            string program_name = "script_loader.py";
+            string program_name = "Scripts/script_loader.py";
             using (Py.GIL())
             {
                 dynamic os = Py.Import("os");
                 dynamic sys = Py.Import("sys");
                 sys.path.append(os.path.dirname(os.path.expanduser(program_name)));
+                Debug.Log("path calculated");
                 dynamic fromFile = Py.Import(Path.GetFileNameWithoutExtension(program_name));
+                Debug.Log("script_loader loaded");
                 fromFile.RunScript(scriptName);
+                Debug.Log("script loaded");
             }
+            return;
         }
-        
-        static Action StartScript(string scriptName)
+        static Action StartScript(Action<string> method, string scriptName)
         {
-            return new Action(() => { RunScript(scriptName); });
+            return () => method(scriptName);
         }
+
+
         // When the world scene is loaded
         private void OnWorld()
         {
